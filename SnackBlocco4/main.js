3e1d8bc7f518375f0b6b9b401b3208fa4ba15fa9
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-05-10 11:45:32 +0200

    Aggiunto Snack 1B

--- SnackBlocco4/main.js
+++ SnackBlocco4/main.js
@@ -1,9 +1,13 @@
-/* SNACK 1a*/
+/* Blocco 4 - SNACK 1A*/
 // Creare un oggetto palla che abbia le seguenti proprietà
 var Palla = {
     nome: 'palla',
-    peso: 10,
-    
+    peso: 10, 
 }
+
+/* Blocco 4 - Snack 1B */
+// Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.
+Palla.peso = Number(prompt('Inserisci il peso:'))
+console.log(Palla.peso);
 
 
